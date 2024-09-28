9d065a46f85ccdaab894f2b32ebb346f10a3a771
Rodomil <rodomil@yandex.ru>
2024-09-28 15:58:03 +0300

    menu

---
+++ PartyGames/Screens/MainView/Cell/CellMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  CellMenuView.swift
+//  PartyGames
+//
+//  Created by Rodomil on 25.09.2024.
+//
+
+import SwiftUI
+
+struct CellMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CellMenuView()
+}
